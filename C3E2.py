d2a18829658bfff8e89a18b329ed4e0031ad23b1
MartinMCodazzi <nahuelcodazzi@gmail.com>
2020-11-30 22:07:57 -0300

    .

--- C3E2.py
+++ C3E2.py
@@ -7,20 +7,25 @@ Martin Nahuel Muñoz Codazzi
 30/11/2020
 
 """
-diccionario = {}
+import sys
 
-def lecturaDiccionario():
-    with open ("archivoC3E2.txt","r") as archivo:
-        for elemento in archivo:
-            txt = archivo.readline()
-            txt = txt.replace(":","")
-            txt = txt.split()
+diccionario = {} #Si no lo creo ahora, luego dará error
+
+def lecturaDiccionario(archivo = "archivoC3E2.txt"):
+    with open (archivo,"r") as archivo:
+        for elemento in archivo:            #Para leer hasta EOF
+            txt = archivo.readline()        #leo una línea
+            txt = txt.replace(":","")       #cambio el ":" por Vacío
+            txt = txt.split()               #Divido ambos términos, generando
+                                            #Una lista de dos elementos
             diccionario.update({txt[0]:txt[1]})
     return(diccionario)
 
 diccionario = lecturaDiccionario()
+#en la llamada a la función, podemos indicarle cualquier archivo
 print(diccionario)
 
+sys.exit(0) #Parecee que esto es buena práctica, y lo puedo usar a futuro
 
-# 30/11 No estoy seguro por qué, pero el archivo, debo pasarlo con un  
-#salto de línea entre los elementos.
+# 30/11 No estoy seguro por qué, pero el archivo, debo pasarlo con un
+#salto de línea entre cada elementos, quizás se deba a los line endings.
