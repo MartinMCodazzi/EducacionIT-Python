639d35c934bedbd06eec635a2bd1560a853c482e
MartinMCodazzi <nahuelcodazzi@gmail.com>
2020-12-02 11:59:10 -0300

    .

--- C3E2.py
+++ C3E2.py
@@ -21,14 +21,15 @@ diccionario = {} #Si no lo creo ahora, luego dará error
 def lecturaDiccionario(archivo = "archivoC3E2.txt"):
     with open (archivo,"r") as archivo:
         bruto = archivo.read() # leo en bruto todo el archivo
-        bruto = bruto.replace(" ","") #elimino espacios por primera vez
-        bruto = bruto.split("\n") #Separo por saltos de linea
-        bruto.pop() #con esto elimino la basura que deja el comando anterior
+    bruto = bruto.replace(" ","") #elimino espacios por primera vez
+    bruto = bruto.split("\n") #Separo por saltos de linea
+    bruto.pop() #con esto elimino la basura que deja el comando anterior
 
-        for elemento in bruto:
-            #creo una lista de dos elementos por cada elemento
-            temporal = elemento.split(":")
-            diccionario.update({temporal[0]:temporal[1]})
+    for elemento in bruto:
+        #creo una lista de dos elementos por cada elemento
+        #también, podría preguntar si me dan una lista vacía
+        temporal = elemento.split(":")
+        diccionario.update({temporal[0]:temporal[1]})
 
     return(diccionario)
 
